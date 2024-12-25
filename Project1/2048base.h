bd1001f7f2c40239d023435f4280f279f732465f
cao <cao18373503328@outlook.com>
2024-12-25 08:28:03 +0800

    2048游戏学习

---
+++ Project1/2048base.h
@@ -0,0 +1,11 @@
+#pragma once
+void AddNum(int* p, int len);
+int combine(int* p, int len, bool rev);
+void delZeros(int* p, int len, bool rev);
+bool ifchange(int* p, int* q, int len);
+void out(int* p, int len);
+bool pileRows(int* p, int len, bool rev, bool trans, int* score);
+void run(int* p, int len, int* score);
+void transform(int* p, int len);
+
+
--- Project1/mian.cpp
+++ Project1/mian.cpp
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <graphics.h>
-int main() {
-	printf("Hello World");
-	return 0;
+#include "2048base.h"
+#include <stdlib.h >
+/*÷˜≥Ã–Ú£¨≤‚ ‘”√*/
+int main()
+{
+    int b[4][4] = { 4, 0, 0, 0,
+        2, 0, 0, 0,
+        8, 0, 0, 0,
+        2, 0, 0, 0 };
+    int *score = new int;
+    *score = 0;
+    int *x = (int *)b;
+    while (1)
+    {
+        run(x, 4, score);
+        system("cls");
+        out(x, 4);
+    }
+    return 0;
 }
+
